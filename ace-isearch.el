61a45226ccbcd907233ec4dc7ac6ef126c9e1abd
Akira Tamamori <tamamori5917@gmail.com>
2014-10-16 15:54:18 +0900

    fix docstring

--- ace-isearch.el
+++ ace-isearch.el
@@ -66,8 +66,8 @@ during isearch."
   :group 'ace-isearch)
 
 (defcustom ace-isearch-input-length 6
-  "Length of inpunt string during isearch which is required to invoke
-`ace-isearch-function-from-isearch'."
+  "Length of inpunt string required for invoking
+`ace-isearch-function-from-isearch' during isearch."
   :type 'integer
   :group 'ace-isearch)
 
