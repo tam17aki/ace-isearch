c29d4fa701a97dabd4e73e3c08d9b8d3dd7c3d1a
Akira Tamamori <tamamori5917@gmail.com>
2015-03-05 11:17:32 +0900

    Remove message

--- ace-isearch.el
+++ ace-isearch.el
@@ -137,9 +137,7 @@ of `isearch-string' is longer than or equal to `ace-isearch-input-length'."
     t))
 
 (defun ace-isearch--pop-mark ()
-  (if (= (length isearch-string) 1)
-      (progn (pop-mark)
-             (message "Mark saved when search started"))))
+  (if (= (length isearch-string) 1) (progn (pop-mark))))
 
 (defun ace-isearch--jumper-function ()
   (cond ((and (= (length isearch-string) 1)
