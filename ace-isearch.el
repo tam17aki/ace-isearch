6c15b574d19b75a7969d8f633261577c420fcd31
Akira Tamamori <tamamori5917@gmail.com>
2016-11-08 10:29:22 +0900

    workaround for emacs 25.1

--- ace-isearch.el
+++ ace-isearch.el
@@ -175,8 +175,8 @@ of `isearch-string' is longer than or equal to `ace-isearch-input-length'."
              (message "Notice: Character '%s' could not be found in the \"selected visible window\"." isearch-string))
          (funcall ace-isearch-function (string-to-char isearch-string))
          ;; work-around for emacs 25.1
-         (setq isearch--current-buffer (buffer-name (current-buffer)))
-         (setq isearch-string ""))
+         (setq isearch--current-buffer (buffer-name (current-buffer))
+               isearch-string ""))
 
         ((and (> (length isearch-string) 1)
               (< (length isearch-string) ace-isearch-input-length)
@@ -194,8 +194,8 @@ of `isearch-string' is longer than or equal to `ace-isearch-input-length'."
          (isearch-exit)
          (funcall ace-isearch-function-from-isearch)
          ;; work-around for emacs 25.1
-         (setq isearch--current-buffer (buffer-name (current-buffer)))
-         (setq isearch-string "")))))
+         (setq isearch--current-buffer (buffer-name (current-buffer))
+               isearch-string ""))))
 
 (defun ace-isearch-pop-mark ()
   "Jump back to the last location of `ace-jump-mode' invoked or `avy-push-mark'."
